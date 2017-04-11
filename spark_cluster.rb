d9ea74ffabc15117b500d96bee80cfb9000e9cd9
Harry Obaseki <harryobas@gmail.com>
2017-04-11 13:17:55 +0200

    fixed passwordless ssh connectivity between cluster nodes

--- spark_cluster.rb
+++ spark_cluster.rb
@@ -30,29 +30,15 @@ def get_ip_address_for_vms vms
 end
 
 def configure_and_start_spark_cluster vm_ips
-  vm_ips.each_with_index do |ip, idx|
-    next if idx == 0
-    configure_spark ip, vm_ips
-  end
+  pub_key = get_master_public_key vm_ips.first
+  vm_ips.each {|vm| set_authorized_key(vm, pub_key)}
   start_spark vm_ips.first
-end
-
- def configure_spark worker_ip, ip_list
-  cmd = <<-eos
-  cd /home/ioi600/spark-2.0.2/conf
-  echo #{worker_ip} >> slaves.template
-  cat /home/ioi600/.ssh/id_rsa.pub | ssh root@#{worker_ip} 'cat >> .ssh/authorized_keys'
-  eos
-  master_ip = ip_list.first
-  ssh = Net::SSH.start("#{master_ip}", 'root')
-  ssh.exec!(cmd)
 end
 
 def start_spark master_ip
   cmd = <<-eos
   cd /home/ioi600/spark-2.0.2/sbin
-  ./start-master.sh
-  ./start-slaves.sh
+  ./start-all.sh
   eos
   ssh = Net::SSH.start("#{master_ip}", 'root')
   res = ssh.exec!(cmd)
@@ -60,11 +46,12 @@ def start_spark master_ip
 end
 
 def start_bandwidth_throttler vm_ips
-  port_nums = Array.new(16){rand(2000..20000)}
-  vm_ips.each do |isp|
-    port = port_nums.shift.to_s
+  port_nums = (200..250).map{|n| n}
+  vm_ips.each do |ip|
+    port = port_nums.shift
     cmd = <<-eos
-    python shape_traffic_server.py --port port
+    cd /home/ioi600/bandwidth-throttler
+    python shape_traffic_server.py --port #{port}
     eos
     ssh = Net::SSH.start("#{ip}", 'root')
     res = ssh.exec!(cmd)
@@ -74,27 +61,26 @@ def start_bandwidth_throttler vm_ips
 def start template, vms_list_file, vm_number
   ips = provision_vms template, vm_number
   File.truncate("#{vms_list_file}", 0) unless File.zero?("#{vms_list_file}")
-  File.open(vms_list_file, 'a') do |f|
-    f.puts "Node\tRole"
-    f.puts "#{ips.first}\tmaster"
-  end
   ips.each_with_index do |ip, idx|
-    cmd = <<-eos
-    mkdir /home/ioi600/.ssh
-    cd /home/ioi600/.ssh
-    touch authorized_keys
-    chmod 0600 authorized_keys
-    eos
-    next if idx == 0
-    Thread.new {File.open("#{vms_list_file}", 'a') do |f|
-      f.puts "#{ip}\worker"
-    end}.join
-    ssh = Net::SSH.start("#{ip}", 'root')
-    ssh.exec!(cmd)
+    File.open("#{vms_list_file}", 'a') do |f|
+      f.puts "#{ip}\tmaster" if idx == 0
+      f.puts "#{ip}\tworker"
+    end
   end
   configure_and_start_spark_cluster ips
-  start_bandwidth_throttler ips
+  Thread.new {start_bandwidth_throttler(ips)}.join
 end
+
+  def get_master_public_key master_node
+    ssh = Net.SSH.start("#{master_node}", "root")
+    result = ssh.exec!("cat /root/.ssh/rsa_id.pub")
+    return result
+  end
+
+  def set_authorized_key node pub_key
+    ssh = Net::SSH.start "#{node}", "root"
+    ssh.exec!("echo #{pub_key} >> /root/.ssh/authorized_keys")
+  end
 
 if __FILE__ == $PROGRAM_NAME
   vm_template = ARGV.shift
